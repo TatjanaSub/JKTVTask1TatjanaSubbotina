fe6f4b66a8c502c01a61ab72915b28f5b67f45e5
pupil <pupil@ivkhk.ee>
2022-09-26 08:30:53 +0300

    fist commit

---
+++ src/jktv21task1tatjanasubbotina/JKTV21Task1TatjanaSubbotina.java
@@ -0,0 +1,19 @@
+/*
+ 
+ */
+package jktv21task1tatjanasubbotina;
+
+/**
+ *
+ * @author pupil
+ */
+public class JKTV21Task1TatjanaSubbotina {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Java Hello!");
+    }
+    
+}
